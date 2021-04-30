3178d15ac59a39ec84e21213247b1657d74afc50
aperdomoll90 <aperdomoll90@gmail.com>
2021-04-30 14:24:15 -0400

    style added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,9 +4,8 @@ import React from 'react'
 function Header({isLoggedIn, setIsLoggedIn}){
 return (
 <>
-<img src="./source.gif" alt="running puppy"/>
-
-<p>Welcome</p>
+<img src='https://media3.giphy.com/media/J1GGJmqOk8DcpCyACR/giphy.gif?cid=790b7611f506ecc12693cdd557f5c5e18afafd7f2078f358&rid=giphy.gif&ct=s'/>
+<h1>Welcome</h1>
 </>
 )
 }

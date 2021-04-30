ffaaf088e8a871e6d6c71b56cfa8fb41ea65c4be
aperdomoll90 <aperdomoll90@gmail.com>
2021-04-30 15:02:14 -0400

    more stiling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
-function Footer() {
-	return <p>Footer</p>
+
+function Footer(){
+return(
+    <p>....Did you find your perfect fur BABY?</p>
+)
 }
 export default Footer
 
